cb90ecdd7555a6420b69f8b23d85b1368a782934
andreseagle1927 <96150493+andreseagle1927@users.noreply.github.com>
2023-09-26 13:07:18 -0500

    Update VistaVehiculo.java

--- src/main/java/dev/andresabadia/observer/VistaVehiculo.java
+++ src/main/java/dev/andresabadia/observer/VistaVehiculo.java
@@ -1,4 +1,24 @@
 package dev.andresabadia.observer;
 
-public class VistaVehiculo {
+public class VistaVehiculo implements Observador
+{
+    protected Vehiculo vehiculo;
+    protected String texto = "";
+    public VistaVehiculo(Vehiculo vehiculo) {
+        this.vehiculo = vehiculo;
+        vehiculo.agrega(this);
+        actualizaTexto();
+    }
+
+    protected void actualizaTexto() {
+        texto = "Descripcion " + vehiculo.descripcion + " Precio: " + vehiculo.precio;
+    }
+
+    public void actualiza() {
+        actualizaTexto();
+        this.redibuja();
+    }
+    public void redibuja() {
+        System.out.println(texto);
+    }
 }
