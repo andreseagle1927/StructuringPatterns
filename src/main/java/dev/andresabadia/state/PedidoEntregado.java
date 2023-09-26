138c0174e17ef44b77e6559f31edb85ad957793d
andreseagle1927 <96150493+andreseagle1927@users.noreply.github.com>
2023-09-26 16:27:40 -0500

    Create PedidoEntregado.java

---
+++ src/main/java/dev/andresabadia/state/PedidoEntregado.java
@@ -0,0 +1,16 @@
+package dev.andresabadia.state;
+
+public class PedidoEntregado extends EstadoPedido
+{
+    public PedidoEntregado(Pedido pedido)
+    {
+        super(pedido);
+    }
+    public void agregaProducto(Producto producto) { }
+    public void borra() { }
+    public void suprimeProducto(Producto producto) { }
+    public EstadoPedido estadoSiguiente()
+    {
+        return this;
+    }
+}
