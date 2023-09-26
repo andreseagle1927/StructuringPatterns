7429850d285f2cc30e6f7230cce20739752ff34b
andreseagle1927 <96150493+andreseagle1927@users.noreply.github.com>
2023-09-26 16:28:45 -0500

    Create Producto.java

---
+++ src/main/java/dev/andresabadia/state/Producto.java
@@ -0,0 +1,14 @@
+package dev.andresabadia.state;
+
+public class Producto
+{
+    protected String nombre;
+    public Producto(String nombre)
+    {
+        this.nombre = nombre;
+    }
+    public void visualiza()
+    {
+        System.out.println("Producto: " + nombre);
+    }
+}
