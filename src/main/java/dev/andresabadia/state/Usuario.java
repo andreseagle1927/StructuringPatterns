29fcc106a1defbbc1cfdaa4889fd380786ab6847
andreseagle1927 <96150493+andreseagle1927@users.noreply.github.com>
2023-09-26 17:05:03 -0500

    final working

--- src/main/java/dev/andresabadia/state/Usuario.java
+++ src/main/java/dev/andresabadia/state/Usuario.java
@@ -7,17 +7,23 @@ public class Usuario
         Pedido pedido = new Pedido();
         pedido.agregaProducto(new Producto("vehiculo 1"));
         pedido.agregaProducto(new Producto("accesorio 2"));
+
         pedido.visualiza();
         pedido.estadoSiguiente();
+
         pedido.agregaProducto(new Producto("accesorio 3"));
+
         pedido.borra();
         pedido.visualiza();
+
         Pedido pedido2 = new Pedido();
         pedido2.agregaProducto(new Producto("vehiculo 11"));
         pedido2.agregaProducto(new Producto("accesorio 21"));
         pedido2.visualiza();
+
         pedido2.estadoSiguiente();
         pedido2.visualiza();
+
         pedido2.estadoSiguiente();
         pedido2.borra();
         pedido2.visualiza();
