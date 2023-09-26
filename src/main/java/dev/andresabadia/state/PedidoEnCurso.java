164ff20d9c2c0fa2ac9a9f0a9d8960c2909b859c
andreseagle1927 <96150493+andreseagle1927@users.noreply.github.com>
2023-09-26 16:23:17 -0500

    state

---
+++ src/main/java/dev/andresabadia/state/PedidoEnCurso.java
@@ -0,0 +1,25 @@
+package dev.andresabadia.state;
+
+public class PedidoEnCurso extends EstadoPedido
+{
+    public PedidoEnCurso(Pedido pedido)
+    {
+        super(pedido);
+    }
+    public void agregaProducto(Producto producto)
+    {
+        pedido.getProductos().add(producto);
+    }
+    public void borra()
+    {
+        pedido.getProductos().clear();
+    }
+    public void suprimeProducto(Producto producto)
+    {
+        pedido.getProductos().remove(producto);
+    }
+    public EstadoPedido estadoSiguiente()
+    {
+        return new PedidoValidado(pedido);
+    }
+}
